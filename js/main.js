a0a983aa00fb7520e7ce530ddc371dbcb7b83e57
Miguel Borges <mrcb08022002@gmail.com>
2022-02-14 21:33:48 +0000

    turn code more functional

--- js/main.js
+++ js/main.js
@@ -20,9 +20,7 @@ const RESULTS = {
 }
 
 
-function computerSelection() {
-    return PLAYABLE_OBJETCS[Math.floor(Math.random() * 3)]
-}
+const computerSelection = () => PLAYABLE_OBJETCS[Math.floor(Math.random() * 3)]
 
 function playerSelection() {
     let pick
@@ -33,22 +31,8 @@ function playerSelection() {
     return pick
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) return `It's a tie! Both picked ${capitalize(playerSelection)}`
-    return (RESULTS[playerSelection][computerSelection]) ? `You won! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`
-                                                         : `You lost. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        roundResult = playRound(playerSelection(), computerSelection());
-        if (roundResult[4] === "w") playerScore++ 
-        else if (roundResult[4] === "l") computerScore++
-        alert(roundResult + `\nScore\n- You: ${playerScore}\n- Computer: ${computerScore}`);
-    }
-    alert( playerScore > computerScore ? "Game over. You've won!" : "You've lost :c. Better luck next time!");
-}
-
-game()
+const playRound = (playerSelection, computerSelection) => playerSelection === computerSelection 
+                                                        ? `It's a tie! Both picked ${capitalize(playerSelection)}`
+                                                        : RESULTS[playerSelection][computerSelection] 
+                                                        ? `You won! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`
+                                                        : `You lost. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
